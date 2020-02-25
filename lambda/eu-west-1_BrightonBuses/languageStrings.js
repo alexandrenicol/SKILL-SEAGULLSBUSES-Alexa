d4914ae9acc7c344f99721ece3f5fc02b4d73474
alexandrenicol <nicol.alexandre1@gmail.com>
2020-02-25 11:52:16 +0000

    extracting language components

---
+++ lambda/eu-west-1_BrightonBuses/languageStrings.js
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+module.exports = {
+  en: {
+    SKILL_NAME: 'Seagulls Buses',
+    HELP_MESSAGE: 'You can ask for the next buses at your home stop, or you can set a new home stop, what can I help you with?',
+    WELCOME_MESSAGE: 'Welcome to the Seagulls Buses skill, you can ask for the next buses at your home stop, or you can set a new home stop, what can I help you with?',
+    LINK_MESSAGE: 'Welcome to the Seagulls Buses skill, you have not set your home bus stop yet, open the Alexa app and click on, Link account, to choose your home stop.',
+    SETSTOP_MESSAGE: 'OK, open the Alexa app and click on, Link account, to change your home stop.',
+    HELP_REPROMPT: 'What can I help you with?',
+    UNHANDLED_MESSAGE: 'Sorry, I didn\'t get that. What can I help you with?',
+    NOBUS_MESSAGE: 'There will be no buses leaving from this stop today.',
+    STOP_MESSAGE: 'Goodbye!',
+    OK: 'OK',
+  },
+};
